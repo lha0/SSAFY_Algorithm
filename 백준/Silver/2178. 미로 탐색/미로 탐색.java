0ee8835023f3f716901ac4244ea2344df0194223
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-14 10:30:23 +0900

    [Silver I] Title: 미로 탐색, Time: 260 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,54 +1,77 @@
-import java.io.*;
+
 import java.util.*;
 
-public class Main {
-	static int N, M;
-	static int[][] maze;
-	static int[] dx = new int[] {0, 0, 1, -1};
-	static int[] dy = new int[] {1, -1, 0, 0};
+class Point2 {
+	int r, c;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String[] inputs = br.readLine().split(" ");
-		N = Integer.parseInt(inputs[0]);
-		M = Integer.parseInt(inputs[1]);
-		
-		maze = new int[N][M];
-		for (int i =0 ; i < N; i++) {
-			String[] input = br.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				maze[i][j] = Integer.parseInt(input[j]);
-			}
-		}
-		
-		bfs();
-		
-		bw.write(maze[N-1][M-1] + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
+	Point2(int r, int c) {
+		this.r = r;
+		this.c = c;
 	}
-	
-	public static void bfs() {
-		Queue<int[]> queue = new ArrayDeque<int[]>();
-		queue.offer(new int[] {0, 0});
-		
-		while (queue.size() != 0) {
-			int[] cur = queue.poll();
-			int x = cur[0];
-			int y = cur[1];
-			
-			for (int idx = 0; idx < 4; idx++) {
-				int nx = x + dx[idx];
-				int ny = y + dy[idx];
-				
-				if (nx >= 0 && nx < N && ny >= 0 && ny < M && maze[nx][ny] == 1) {
-					maze[nx][ny] = maze[x][y] + 1;
-					queue.offer(new int[] {nx, ny});
+}
+
+public class Main {
+
+	static int[][] map;
+	static int n, m;
+	static boolean[][] v;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		m = sc.nextInt();
+		map = new int[n][m];
+		v = new boolean[n][m];
+
+		for (int i = 0; i < n; i++) {
+			String input = sc.next();
+			for (int j = 0; j < input.length(); j++) {
+				char c = input.charAt(j);
+				map[i][j] = Integer.parseInt(String.valueOf(c));
+			}
+
+		}
+
+		System.out.println(bfs(0, 0) - 1);
+
+	}
+
+	private static int bfs(int r, int c) {
+		int cnt = 1;
+		Queue<Point2> q = new ArrayDeque();
+		q.offer(new Point2(r, c));
+		v[r][c] = true;
+		map[r][c] = 1;
+		cnt++;
+
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				Point2 p = q.poll();
+
+				if (p.r == n - 1 && p.c == m - 1) {
+					return cnt;
+				}
+
+				for (int j = 0; j < 4; j++) {
+					int nr = p.r + dr[j];
+					int nc = p.c + dc[j];
+
+					if (nr >= 0 && nr < n && nc >= 0 && nc < m && map[nr][nc] == 1 && !v[nr][nc]) {
+						map[nr][nc] = cnt;
+						q.offer(new Point2(nr, nc));
+						v[nr][nc] = true;
+					}
 				}
 			}
+			cnt++;
 		}
+
+		return cnt;
+
 	}
+
 }
