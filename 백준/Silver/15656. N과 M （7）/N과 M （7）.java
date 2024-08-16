f1cea4f8db5bfbb305e7b936173b8c180af4d7c4
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-16 22:30:15 +0900

    [Silver III] Title: N과 M (7), Time: 788 ms, Memory: 301156 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int[] origin;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		StringTokenizer sts = new StringTokenizer(br.readLine());
+		origin = new int[N];
+		for (int i = 0; i < N; i++) {
+			origin[i] = Integer.parseInt(sts.nextToken());
+		}
+		Arrays.sort(origin);
+		
+		comb( 0, new int[M], bw);
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	public static void comb(int k, int[] sel, BufferedWriter bw )  throws IOException {
+		if (sel.length == k) {
+			for (int i = 0; i < k; i++) {
+				bw.write(sel[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			sel[k] = origin[i];
+			comb(k+1, sel, bw);
+		}
+	}
+
+}
