bf914487a31d04817ae56b6400d48d8e17907113
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-16 22:44:44 +0900

    [Silver II] Title: N과 M (11), Time: 472 ms, Memory: 214564 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -4,34 +4,33 @@ import java.util.*;
 public class Main {
 	static int N, M;
 	static int[] origin;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String[] inputs = br.readLine().split(" ");
-		N = Integer.parseInt(inputs[0]);
-		M = Integer.parseInt(inputs[1]);
-		
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		StringTokenizer sts = new StringTokenizer(br.readLine());
 		origin = new int[N];
-		String[] input = br.readLine().split(" ");
 		for (int i = 0; i < N; i++) {
-			origin[i] =  Integer.parseInt(input[i]);
+			origin[i] = Integer.parseInt(sts.nextToken());
 		}
 		Arrays.sort(origin);
-		
-		int[] sel = new int[M];
-		boolean[] visited = new boolean[N];
-		comb( 0, sel, visited, bw);
-		
+
+		comb( 0, new int[M],  bw);
+
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-	
-	public static void comb( int k, int[] sel, boolean[] visited, BufferedWriter bw )  throws IOException {
-		if (k == sel.length) {
-			for (int j  = 0; j < sel.length; j++) {
-				bw.write(sel[j] + " ");
+
+	public static void comb(int k, int[] sel,  BufferedWriter bw) throws IOException {
+		if (sel.length == k) {
+			for (int i = 0; i < k; i++) {
+				bw.write(sel[i] + " ");
 			}
 			bw.write("\n");
 			return;
@@ -39,14 +38,14 @@ public class Main {
 		
 		int before = 0;
 		for (int i = 0; i < N; i++) {
-				if (before != origin[i]) {
-					sel[k] = origin[i];
-					before = origin[i];
-					comb(k+1, sel, visited, bw);
-				}
-				
+			if (before != origin[i]) {
+				sel[k] = origin[i];
+				before = origin[i];
+				comb(k + 1, sel,  bw);
+			}
+
 		}
-		
+
 	}
 
 }
