7de6682980d3e666f5f65d3cdb2d717256d9e967
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-16 22:19:18 +0900

    [Silver III] Title: N과 M (3), Time: 596 ms, Memory: 296768 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -8,28 +8,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String[] inputs = br.readLine().split(" ");
-		N = Integer.parseInt(inputs[0]);
-		M = Integer.parseInt(inputs[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
 		origin = new int[N];
 		for (int i = 0; i < N; i++) {
 			origin[i] = i+1;
 		}
 		
-		br.close();
-		
-		int[] sel = new int[M];
-		comb(0, sel, bw);
+		comb(0, new int[M], bw);
 		
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 	
-	public static void comb(int k, int[] sel, BufferedWriter bw) throws IOException {
-		
-		if (k == sel.length ) {
-			for (int i = 0; i < sel.length; i++) {
+	public static void comb( int k, int[] sel, BufferedWriter bw )  throws IOException {
+		if (sel.length == k) {
+			for (int i = 0; i < k; i++) {
 				bw.write(sel[i] + " ");
 			}
 			bw.write("\n");
@@ -41,4 +38,5 @@ public class Main {
 			comb(k+1, sel, bw);
 		}
 	}
+
 }
