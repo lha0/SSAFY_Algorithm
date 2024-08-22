13e186882954df818f665e2c1b2042f827511d4e
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-22 17:53:54 +0900

    [Gold IV] Title: N-Queen, Time: 2524 ms, Memory: 12584 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,61 +1,66 @@
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static boolean[][] v;
-	static int result = 0;
+	static int N, answer;
+	static int[] col;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		N = sc.nextInt();
-		v = new boolean[N][N];
-
-		dfs(0);
-		System.out.println(result);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine().split(" ")[0]);
+		
+		boolean[] colCheck = new boolean[N];
+		boolean[] diff = new boolean[2*N];
+		boolean[] sum = new boolean[2*N];
+		queen(0, colCheck, diff, sum);
+		
+		bw.write(answer + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
 
 	}
-
-	private static void dfs(int cnt) {
-		if (cnt == N) {
-			result++;
+	
+	static public void queen(int row, boolean[] colCheck, boolean[] diff, boolean[] sum) {
+		if (row == N-1) {
+			for (int c = 0; c < N; c++) {
+				int diffNum = (row - c) + N;
+				int sumNum = (row+c);
+				
+				//열 체크
+				if (!colCheck[c] && !diff[diffNum] && !sum[sumNum]) {
+					answer++;
+				}
+			}
+		
 			return;
 		}
-		for( int i = 0 ; i < N ; i++) {
-			if(check(cnt , i)) {
-				v[cnt][i]=true;
-				dfs(cnt+1);
+		
+		//놓으려는 자리 = c = 열
+		for (int c = 0; c < N; c++) {
+			
+			int diffNum = (row - c) + N;
+			int sumNum = (row+c);
+			
+			//열 체크
+			if (!colCheck[c] && !diff[diffNum] && !sum[sumNum]) {
+				colCheck[c] = true;
+				diff[diffNum] = true;
+				sum[sumNum] = true;
+				
+				queen(row+1, colCheck, diff, sum);
+				
+				colCheck[c] = false;
+				diff[diffNum] = false;
+				sum[sumNum] = false;
 			}
-			v[cnt][i]= false;
 			
 		}
-	}
-
-	private static boolean check(int y, int x) {
-
-		// 위 , 대각선 왼쪽 대각선 오른쪽만 검사하면 됨
-
-		// 위 검사 y 값이 계속 -된다
-		for (int i = y; i >= 0; i--) {
-			if (v[i][x]) {
-				return false;
-			}
-		}
-
-		// 대각선 위 검사
-		for (int i = y, j = x; i >= 0 && j >= 0; i--, j--) {
-			if (v[i][j]) {
-				return false;
-			}
-		}
-
-		for (int i = y, j = x; i >= 0 && j < N; i--, j++) {
-			if (v[i][j]) {
-				return false;
-			}
-		}
-
-		return true;
+		
+		
 	}
 
 }
