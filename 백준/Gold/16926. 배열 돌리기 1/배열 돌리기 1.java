48537e8bd1009e95f1a9d06c386535b17379d12a
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-22 13:30:54 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 528 ms, Memory: 40516 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -13,7 +13,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
+
 		arr = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer sts = new StringTokenizer(br.readLine());
@@ -21,24 +21,22 @@ public class Main {
 				arr[i][j] = Integer.parseInt(sts.nextToken());
 			}
 		}
-		
+
 		turn();
-		
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				bw.write(arr[i][j] + " ");
 			}
 			bw.write("\n");
 		}
-		
+
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-	
+
 	public static void turn() {
-//		R = R % ((N+M) * 2 - 4);
-		
 		for (int r = 0; r < R; r++) {
 			int sx = 0;
 			int sy = 0;
@@ -46,69 +44,54 @@ public class Main {
 			int height = M;
 			int nx = 0;
 			int ny = 0;
-			
-			//줄이기
+
+			// 줄이기
 			while (width > 1 && height > 1) {
 				int initial = arr[nx][ny];
-				
-				//윗변
+
+				// 윗변
 				int cnt = 0;
-				while (cnt < height-1) {
-					arr[nx][ny] = arr[nx][ny+1];
+				while (cnt < height - 1) {
+					arr[nx][ny] = arr[nx][ny + 1];
 					ny++;
 					cnt++;
 				}
-				
-				//오른쪽
+
+				// 오른쪽
 				cnt = 0;
-				while (cnt < width-1) {
-					arr[nx][ny] = arr[nx+1][ny];
+				while (cnt < width - 1) {
+					arr[nx][ny] = arr[nx + 1][ny];
 					nx++;
 					cnt++;
 				}
-				
-				//아래
+
+				// 아래
 				cnt = 0;
-				while (cnt < height-1) {
-					arr[nx][ny] = arr[nx][ny-1];
+				while (cnt < height - 1) {
+					arr[nx][ny] = arr[nx][ny - 1];
 					ny--;
 					cnt++;
 				}
-				
-				//왼쪽
+
+				// 왼쪽
 				cnt = 0;
-				while (cnt < width-2) {
-					arr[nx][ny] = arr[nx-1][ny];
+				while (cnt < width - 2) {
+					arr[nx][ny] = arr[nx - 1][ny];
 					nx--;
 					cnt++;
 				}
-				
+
 				arr[nx][ny] = initial;
-				
-				nx = sx+1;
-				ny = sy+1;
-				
+
+				nx = sx + 1;
+				ny = sy + 1;
+
 				width = width -= 2;
 				height = height -= 2;
-				
+
 				sx = nx;
 				sy = ny;
-				
-//				System.out.println(nx + " " + ny + " " + width + " " + height);
-				
-//				System.out.println(r);
-//				for (int i = 0; i < N; i++) {
-//					for (int j = 0; j < M; j++) {
-//						System.out.print(arr[i][j] + " ");
-//					}
-//					System.out.println("");
-//				}
-//				System.out.println();
-				
 			}
-			
-			
 		}
 	}
-
 }
