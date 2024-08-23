2a614c4baf2b81bbe3097b5ed03df5564475a334
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-23 14:59:58 +0900

    [D3] Title: 상호의 배틀필드, Time: 151 ms, Memory: 28,284 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -2,148 +2,193 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int T, H, W, N, longer;
-	
-	//위, 아래, 왼, 오
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static int T, H, W, N;
+	static String[][] board;
+	static String[] command;
+	static Tank tank;
+
+	static class Tank {
+		int r, c;
+		String dir;
+
+		Tank(int r, int c, String dir) {
+			this.r = r;
+			this.c = c;
+			this.dir = dir;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String input = br.readLine();
-		T = Integer.parseInt(input);
-		
-		for (int t = 1; t <= T; t++) {
-			String[] HW = br.readLine().split(" ");
-			H = Integer.parseInt(HW[0]);
-			W = Integer.parseInt(HW[1]);
-			
-			longer = Math.max(H, W);
-			
-			String[][] map = new String[H][W];
-			
-			for (int i = 0; i < H; i++) {
-				map[i] = br.readLine().split("");
-			}
-			
-			N = Integer.parseInt(br.readLine());
-			
-			String[] command = new String[N];
-			command = br.readLine().split("");
-			
-			int curX = 0;
-			int curY = 0;
-			L: for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
-					if (map[i][j].equals("<") || map[i][j].equals(">") || map[i][j].equals("^") || map[i][j].equals("v")) {
-						curX = i;
-						curY = j;
-						i = H;
-						break L;
-					}
-				}
-			}
-			
-			for (int i = 0; i < N; i++) {
-				String curSee = map[curX][curY];
-				//System.out.println(command[i] + " " + curSee);
-				
-				switch(command[i]) {
-				case "U":
-					map[curX][curY] = "^";
-					if (curX - 1 >= 0 && map[curX-1][curY].equals(".")) {
-						map[curX-1][curY] = "^";
-						map[curX][curY] = ".";
-						curX = curX-1;
-					}
-					break;
-					
-				case "D":
-					map[curX][curY] = "v";
-					if (curX + 1 < H && map[curX+1][curY].equals(".")) {
-						map[curX+1][curY] = "v";
-						map[curX][curY] = ".";
-						curX = curX+1;
-					}
-					break;
-					
-				case "L":
-					map[curX][curY] = "<";
-					if (curY - 1 >= 0 && map[curX][curY - 1].equals(".")) {
-						map[curX][curY - 1] = "<";
-						map[curX][curY] = ".";
-						curY = curY - 1;
-					}
-					break;
-					
-				case "R":
-					map[curX][curY] = ">";
-					if (curY+1 < W && map[curX][curY+1].equals(".")) {
-						map[curX][curY+1] = ">";
-						map[curX][curY] = ".";
-						curY = curY+1;
-					}
-					break;
-					
-				case "S":
-					switch(curSee) {
-					case "<":
-						shoot(0, -1, curX, curY, map);
-						break;
-					case ">":
-						shoot(0, 1, curX, curY, map);
-						break;
-					case "^":
-						shoot(-1, 0, curX, curY, map);
-						break;
-					case "v":
-						shoot(1, 0, curX, curY, map);
-						break;
-					}
-					break;
-				}
-				
-//				for (int x = 0; x < H; x++) {
-//					for (int y = 0; y < W; y++) {
-//						System.out.print(map[x][y]);
-//					}
-//					System.out.println();
-//				}
-//				System.out.println();
-	
-			
-			}
-			
-			System.out.print("#" + t + " ");
-			for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
-					System.out.print(map[i][j]);
-				}
-				System.out.println();
-			}
-//			System.out.println();
-		}
-		
 
+		T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			String[] inputs = br.readLine().split(" ");
+			H = Integer.parseInt(inputs[0]);
+			W = Integer.parseInt(inputs[1]);
+
+			board = new String[H][W];
+
+			for (int i = 0; i < H; i++) {
+				String[] input = br.readLine().split("");
+				for (int j = 0; j < W; j++) {
+					board[i][j] = input[j];
+
+					if (board[i][j].equals("<")) {
+						tank = new Tank(i, j, "<");
+					} else if (board[i][j].equals(">")) {
+						tank = new Tank(i, j, ">");
+					} else if (board[i][j].equals("^")) {
+						tank = new Tank(i, j, "^");
+					} else if (board[i][j].equals("v")) {
+						tank = new Tank(i, j, "v");
+					}
+				}
+			}
+
+			N = Integer.parseInt(br.readLine().split(" ")[0]);
+			command = new String[N];
+			String[] inputc = br.readLine().split("");
+			for (int i = 0; i < N; i++) {
+				command[i] = inputc[i];
+			}
+			
+
+			run();
+			
+			bw.write("#" + tc + " ");
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					bw.write(board[i][j]);
+				}
+				bw.write("\n");
+			}
+
+			
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void run() {
+		for (int c = 0; c < command.length; c++) {
+			switch (command[c]) {
+			case "U":
+				up();
+				break;
+			case "D":
+				down();
+				break;
+			case "L":
+				left();
+				break;
+			case "R":
+				right();
+				break;
+			case "S":
+				shoot();
+				break;
+			default: break;
+			}
+			
+		}
 	}
 	
-	public static void shoot(int i, int j, int curX, int curY, String[][] map) {
-		for (int d = 1; d <= longer; d++) {
-			int nx = curX + i*d;
-			int ny = curY + j*d;
-			
-			if (nx < 0 || ny < 0 || nx >= H || ny >= W || map[nx][ny].equals("#")) {
+	
+	private static void shoot() {
+		int dx = 0; int dy = 0;
+		int mult = 1;
+		
+		while (true) {
+			switch(tank.dir) {
+			case ">":
+				dx = 0;
+				dy = 1;
 				break;
-			} else if (map[nx][ny].equals("*")) {
-				map[nx][ny] = ".";
+			case "<":
+				dx = 0;
+				dy = -1;
 				break;
-			} else {
-				continue;
+			case "^":
+				dx = -1;
+				dy = 0;
+				break;
+			case "v":
+				dx = 1;
+				dy = 0;
+				break;
 			}
+			
+			int nx = tank.r + dx * mult;
+			int ny = tank.c + dy * mult;
+			
+			
+			if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+				if (board[nx][ny].equals("#")) break;
+				else if (board[nx][ny].equals("*")) {
+					board[nx][ny] = ".";
+					mult++;
+					break;
+				} else {
+					mult++;
+					continue;
+				}
+			} else break;
+			
 		}
-		return;
+	}
+
+	private static void right() {
+		tank.dir = ">";
+		board[tank.r][tank.c] = ">";
+
+		// 평지면 이동
+		if (tank.c + 1 < W && board[tank.r][tank.c + 1].equals(".")) {
+			 board[tank.r][tank.c + 1] = ">";
+			board[tank.r][tank.c] = ".";
+			tank.c += 1;
+		}
+	}
+
+	private static void left() {
+		tank.dir = "<";
+		board[tank.r][tank.c] = "<";
+
+		// 평지면 이동
+		if (tank.c - 1 >= 0 && board[tank.r][tank.c - 1].equals(".")) {
+			 board[tank.r][tank.c - 1] = "<";
+			board[tank.r][tank.c] = ".";
+			tank.c -= 1;
+		}
+	}
+
+	private static void down() {
+		tank.dir = "v";
+		board[tank.r][tank.c] = "v";
+
+		// 평지면 이동
+		if (tank.r + 1 < H && board[tank.r + 1][tank.c].equals(".")) {
+			board[tank.r + 1][tank.c] = "v";
+			board[tank.r][tank.c] = ".";
+			tank.r += 1;
+		}
+		
+	}
+
+	private static void up() {
+		tank.dir = "^";
+		board[tank.r][tank.c] = "^";
+
+		// 평지면 이동
+		if (tank.r - 1 >= 0 && board[tank.r - 1][tank.c].equals(".")) {
+			board[tank.r - 1][tank.c] = "^";
+			board[tank.r][tank.c] = ".";
+			tank.r -= 1;
+		}
 	}
 
 }
