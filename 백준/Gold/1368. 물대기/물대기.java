1a188e1fefadf417bb97423630fe4676417abc07
이하영 <78598160+lha0@users.noreply.github.com>
2024-08-27 13:19:41 +0900

    [Gold II] Title: 물대기, Time: 232 ms, Memory: 25500 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.java
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -1,16 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+/*
+0번노드부터 시작해서 0번에서의 각 정점으로의 MST를 구현
+*/
+
 public class Main {
 	static int N, answer;
 	static int[] self;
 	static List<Pond>[] adjList;
-	static List<Integer> minIdx;
-	static Queue<Pond> pq = new PriorityQueue<Pond>();
-	
+
 	static class Pond implements Comparable<Pond> {
 		int idx, cost;
-		
+
 		Pond(int idx, int cost) {
 			this.idx = idx;
 			this.cost = cost;
@@ -20,57 +22,43 @@ public class Main {
 		public int compareTo(Pond o) {
 			return this.cost - o.cost;
 		}
-		
-		
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		N = Integer.parseInt(br.readLine().split(" ")[0]);
-		
-		//자기 자신 값
-		self = new int[N];
-		for (int i = 0; i < N; i++) {
-			self[i] = Integer.parseInt(br.readLine().split(" ")[0]);
-		}
-		
-		//인접리스트 초기화
-		adjList = new ArrayList[N];
-		for (int i = 0; i < N; i++) {
+
+		// 인접리스트 초기화
+		adjList = new ArrayList[N+1];
+		for (int i = 0; i < N+1; i++) {
 			adjList[i] = new ArrayList<>();
 		}
-		
-		//인풋 받기
+
+		// 0번 노드 만들기
+		for (int i = 0; i < N; i++) {
+		  int in = Integer.parseInt(br.readLine().split(" ")[0]);
+		  adjList[0].add(new Pond(i+1, in));
+		}
+
+		// 인풋 받기
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				int val = Integer.parseInt(st.nextToken());
-				if (val == 0) {
-					adjList[i].add(new Pond(i, self[i]));
-				} else {
-					adjList[i].add(new Pond(j, val));
-				}
+				if (val != 0) {
+					adjList[i+1].add(new Pond(j+1, val));
+				} 
 			}
 		}
-		
-//		minIdx = new ArrayList<>();
-//		int minVal = Integer.MAX_VALUE;
-//		//직접 물 파는 비용 중 최솟값 찾기
-//		for (int i = 0 ; i < N; i++) {
-//			if (minVal >= self[i]) {
-//				minIdx.add(i);
-//			}
-//		}
-		
-		for (int  i = 0; i < self.length; i++) {
-			pq.offer(new Pond(i, self[i]));
-		}
+
+		self = new int[N+1];
+		Arrays.fill(self, Integer.MAX_VALUE);
 		
 		prim();
-		
-		for (int i = 0; i < self.length; i++) {
+
+		for (int i = 1; i < self.length; i++) {
 			answer += self[i];
 		}
 
@@ -82,26 +70,28 @@ public class Main {
 	}
 
 	private static void prim() {
+		Queue<Pond> pq = new PriorityQueue<Pond>();
+		boolean[] visited = new boolean[N+1];
 		
-		boolean[] visited = new boolean[N];
-		
+		pq.offer(new Pond(0, 0));
+
 		while (!pq.isEmpty()) {
 			Pond cur = pq.poll();
-			
-			if (visited[cur.idx]) continue;
-			
+
+			if (visited[cur.idx])
+				continue;
+
 			visited[cur.idx] = true;
-			
+
 			for (Pond next : adjList[cur.idx]) {
+				
 				if (!visited[next.idx] && self[next.idx] > next.cost) {
 					pq.offer(new Pond(next.idx, next.cost));
 					self[next.idx] = next.cost;
 				}
 			}
-			
+
 		}
-		
-//		System.out.println(Arrays.toString(self));
 	}
-	
+
 }
