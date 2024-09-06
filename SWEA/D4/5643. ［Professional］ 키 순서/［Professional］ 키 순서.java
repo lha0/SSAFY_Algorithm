949375cf736a93e4872330387258a511a5ffe8ad
이하영 <78598160+lha0@users.noreply.github.com>
2024-09-06 11:04:34 +0900

    [D4] Title: [Professional] 키 순서, Time: 658 ms, Memory: 90,896 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,20 +2,22 @@
 import java.util.*;
 import java.io.*;
 
+//중복된 탐색을 하지 않는 버전
+
 public class Solution {
 	static int N, adjMatrix[][], cnt;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int TC = Integer.parseInt(in.readLine());
-		
-		for (int tc = 1; tc <= TC; tc++ ) {
+
+		for (int tc = 1; tc <= TC; tc++) {
 			N = Integer.parseInt(in.readLine());
 			int M = Integer.parseInt(in.readLine());
-			
-			adjMatrix = new int[N+1][N+1]; // 학생 번호 1번부터 시작
+
+			adjMatrix = new int[N + 1][N + 1]; // 학생 번호 1번부터 시작
 			StringTokenizer st = null;
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(in.readLine());
@@ -23,39 +25,55 @@ public class Solution {
 				int b = Integer.parseInt(st.nextToken());
 				adjMatrix[a][b] = 1;
 			}
-			
-			int ans = 0; //자신의 키를 알 수 있는 학생 수
-			//각 학생마다 자신보다 큰, 자신보다 작은 학생 각각 탐색
+
 			for (int i = 1; i <= N; i++) {
-				cnt = 0;
-				gtDFS(i, new boolean[N+1]);
-				ltDFS(i, new boolean[N+1]);
-				if(cnt == N-1)	ans++; 
+				adjMatrix[i][0] = -1; // 탐색되지 않은 학생을 나타냄 (후에 탐색이 완료되면 자신보다 큰 학생 수 저장)
+			}
+
+			// 각 학생마다 자신보다 큰, 자신보다 작은 학생 각각 탐색
+			for (int i = 1; i <= N; i++) {
+				if (adjMatrix[i][0] != -1) continue;
+				gtDFS(i);
 			}
 			
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					adjMatrix[0][j] += adjMatrix[i][j];
+				}
+			}
+			
+			int ans = 0; // 자신의 키를 알 수 있는 학생 수
+			for (int k = 1; k <= N; k++) {
+				if (adjMatrix[k][0] + adjMatrix[0][k] == N-1) ans++;
+			}
+			
+
 			bw.write("#" + tc + " " + ans + "\n");
 		}
 		in.close();
 		bw.close();
 	}
-	
-	private static void gtDFS(int cur, boolean[] visited) { //자신보다 큰 학생따라 탐색
-		visited[cur] = true;
+
+	private static void gtDFS(int cur) { // 자신보다 큰 학생따라 탐색
 		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && adjMatrix[cur][i] != 0) {
-				gtDFS(i, visited);
-				cnt++; // 나보다 큰 대상 카운팅
+			if (adjMatrix[cur][i] == 0) continue;
+			if (adjMatrix[i][0] == -1) { // 탐색되지 않은 학생이므로 탐색
+				gtDFS(i);
+			}
+			
+			//나보다 키가 큰 학생이 탐색을 완료한 상태
+			// i보다 키가 큰 학생이 있다면 그 학생들의 정보를 cur에게 반영 (간접 관계를 직접 관계로 경로 압축!)
+			if (adjMatrix[i][0] > 0) {
+				for (int j = 1; j <= N; j++) {
+					if (adjMatrix[i][j] != 0) adjMatrix[cur][j] = 1;
+					
+				}
 			}
 		}
-	}
-	
-	private static void ltDFS(int cur, boolean[] visited) { //자신보다 작은 학생따라 탐색
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && adjMatrix[i][cur] != 0) {
-				ltDFS(i, visited);
-				cnt++; // 나보다 작은 대상 카운팅
-			}
+		
+		adjMatrix[cur][0] = 0; //초깃값이 -1이므로 누적 위해 0으로 초기화
+		for (int k = 1; k <= N; k++) {
+			adjMatrix[cur][0] += adjMatrix[cur][k];
 		}
 	}
 }
