655411d27953d8dc6fe6ddd65a16c663c2ce4296
이하영 <78598160+lha0@users.noreply.github.com>
2024-09-19 11:33:22 +0900

    [Gold V] Title: 퇴사 2, Time: 2124 ms, Memory: 311644 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,43 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	static int N, answer;
-	static int[] T, P, mem;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[N+1];
+		int maxProfit = 0;
+		int currMaxProfit = 0;
 		
-		N = Integer.parseInt(br.readLine().split(" ")[0]);
-		T = new int[N+1];
-		P = new int[N+1];
-		
+		// '오늘: i+1일'이라고 생각하고 계산
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			T[i+1] = Integer.parseInt(st.nextToken());
-			P[i+1] = Integer.parseInt(st.nextToken());
+			int T = sc.nextInt();   // i번째 날 부터 상담이 걸리는 시간
+			int P = sc.nextInt();   // 상담했을 때 받을 수 있는 금액
+			
+			if (i + T <= N && dp[i + T] < currMaxProfit + P) {   // dp[0] = 0 이기 때문에 1일차에도 인덱스 에러가 발생하지 않음
+				dp[i + T] = currMaxProfit + P;   // 우항: 전날까지 최대한 번 금액에 오늘부터 벌 수 있는 금액을 더한 것
+				maxProfit = dp[i + T] > maxProfit ? dp[i + T] : maxProfit;
+			}
+
+			// 오늘까지 번 최대이익 갱신하기
+			currMaxProfit = dp[i + 1] > currMaxProfit ? dp[i + 1] : currMaxProfit;
 		}
 		
-		answer = 0;
-		mem = new int[N+2];
-		
-		run();
-		
-		bw.write(mem[N+1] + "\n");
-		bw.close();
-		br.close();
+		System.out.println(maxProfit);
+		sc.close();
 	}
-
-	private static void run() {
-		for (int i = 1; i <= N; i++) {
-			mem[i] = Math.max(mem[i-1], mem[i]);
-			if (i + T[i] <= N+1)
-				mem[i + T[i]] = Math.max(mem[i + T[i]], mem[i] + P[i]);
-			if (i == N) 
-				mem[N+1] = Math.max(mem[N], mem[N+1]);
-		}
-	}
-
-
 }
