426cac070d32be535ede7f55c547a8405aa8c50f
이하영 <78598160+lha0@users.noreply.github.com>
2024-10-05 00:28:39 +0900

    [Silver II] Title: N과 M (11), Time: 460 ms, Memory: 215596 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -1,51 +1,53 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N, M;
-	static int[] origin;
-
+	static int[] arr;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
-		StringTokenizer sts = new StringTokenizer(br.readLine());
-		origin = new int[N];
+		
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			origin[i] = Integer.parseInt(sts.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(origin);
-
-		comb( 0, new int[M],  bw);
-
-		bw.flush();
+		
+		Arrays.sort(arr);
+		
+		run(0, new int[M], bw);
+		
 		bw.close();
 		br.close();
 	}
 
-	public static void comb(int k, int[] sel,  BufferedWriter bw) throws IOException {
-		if (sel.length == k) {
-			for (int i = 0; i < k; i++) {
+	private static void run(int idx, int [] sel, BufferedWriter bw) throws IOException {
+		if (sel.length == idx) {
+			for (int i = 0; i < sel.length; i++) {
 				bw.write(sel[i] + " ");
 			}
 			bw.write("\n");
+
 			return;
 		}
 		
 		int before = 0;
-		for (int i = 0; i < N; i++) {
-			if (before != origin[i]) {
-				sel[k] = origin[i];
-				before = origin[i];
-				comb(k + 1, sel,  bw);
+		for (int i = 0; i < arr.length; i++) {
+			if (before == arr[i]) continue;
+			else {
+				sel[idx] = arr[i];
+				before = arr[i];
+				run(idx+1, sel, bw);
 			}
-
+			
 		}
-
 	}
 
+	
 }
