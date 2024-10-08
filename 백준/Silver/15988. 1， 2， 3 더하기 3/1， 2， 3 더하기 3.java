e48dc7ea8c8eb9212697bd8df99fda461c0967f9
이하영 <78598160+lha0@users.noreply.github.com>
2024-10-08 09:24:27 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 676 ms, Memory: 306044 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+	static int T, num;
+	static long [] mem;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		T = Integer.parseInt(br.readLine().split(" ")[0]);
+		
+		for (int tc = 1; tc <= T; tc++) {
+			num = Integer.parseInt(br.readLine().split(" ")[0]);
+			
+			mem = new long[num+3];
+			mem[1] = 1;
+			mem[2] = 2;
+			mem[3] = 4;
+			
+			for (int i = 4; i <= num; i++) {
+				mem[i] = (mem[i-3] + mem[i-2] + mem[i-1]) % 1000000009;
+			}
+			
+			bw.write(mem[num] + "\n");
+		}
+		
+		bw.close();
+		br.close();
+	}
+
+}
