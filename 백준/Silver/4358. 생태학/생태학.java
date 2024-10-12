513df79e919f723d2a5b2fa224257d6ecde3a693
이하영 <78598160+lha0@users.noreply.github.com>
2024-10-12 22:59:23 +0900

    [Silver II] Title: 생태학, Time: 664 ms, Memory: 90792 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -7,27 +7,35 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+
 		String line = "";
-		while((line = br.readLine()) != null) {
+		while ((line = br.readLine()) != null) {
 			String tree = line;
 			total++;
-			if (treeMap.containsKey(tree)) treeMap.put(tree, treeMap.get(tree) + 1);
-			else treeMap.put(tree, 1);
+			if (treeMap.containsKey(tree))
+				treeMap.put(tree, treeMap.get(tree) + 1);
+			else
+				treeMap.put(tree, 1);
 		}
-		
-		//정렬 1
-		Map<String, Integer> sortedMap = new TreeMap<>(treeMap);
-		
-//		//정렬 2
-//		List<String> sortedKey = new ArrayList<>(treeMap.keySet());
-//		Collections.sort(sortedKey);
-		
-		sortedMap.forEach((key, value) -> {
+
+//		//정렬 1
+//		Map<String, Integer> sortedMap = new TreeMap<>(treeMap);
+
+//		sortedMap.forEach((key, value) -> {
+//			System.out.printf("%s %.4f\n", key, (double) value / total * 100);
+//		});
+
+		// 정렬 2
+		List<String> sortedKey = new ArrayList<>(treeMap.keySet());
+		Collections.sort(sortedKey);
+
+		for (int i = 0; i < sortedKey.size(); i++) {
+			String key = sortedKey.get(i);
+			int value = treeMap.get(key);
+
 			System.out.printf("%s %.4f\n", key, (double) value / total * 100);
-		});
-		
+		}
+
 	}
 
 }
