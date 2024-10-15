091fbf69e6aa4411271c9cec4a9b63263f4c3786
이하영 <78598160+lha0@users.noreply.github.com>
2024-10-15 13:04:41 +0900

    [Gold V] Title: 틱택토, Time: 68 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -15,6 +15,7 @@ public class Main {
 			Xcnt = 0;
 			Ocnt = 0;
 			maps = new String[3][3];
+			
 			for (int i = 0; i < arr.length; i++) {
 				String cur = arr[i];
 
@@ -25,36 +26,42 @@ public class Main {
 				else if (cur.equals("O"))
 					Ocnt++;
 			}
-
+			
+			//두 개 개수 같을 때
 			 if (Xcnt == Ocnt) {
+				 //아직 더 채울 수 있는 경우 invalid
 				if (Xcnt <= 2 && Ocnt <= 2) {
 					bw.write("invalid\n");
 				}
 				else {
+					//X랑 O가 둘 다 3줄 채워져있는 경우 invalid
 					if ((row("X") && row("O")) || (col("X") && col("O")) || (diagonal("X") && diagonal("O"))) {
 						bw.write("invalid\n");
 					}
-					// 가로 판별
+					// O가 다 채워지면서 끝난 경우 valid
 					else if (row("O") || col("O") || diagonal("O")) {
 						bw.write("valid\n");
 					}
 					
 					else {
 						bw.write("invalid\n");
-						
 					}
 				}	
 			} 
-			
+			 
+			// O가 놓을 차례 
 			else if (Xcnt == Ocnt + 1) {
+				//O가 다 채워져있는 경우
 				if (row("O") || col("O") || diagonal("O")) {
 					bw.write("invalid\n");
 				} 
 				
+				//X가 3줄 완성한 경우 valid
 				else if (Xcnt >= 3 && (row("X") || col("X") || diagonal("X"))) {
 					bw.write("valid\n");
 				}
 				
+				//게임판이 다 채워진 경우
 				else if (Xcnt + Ocnt == 9) {
 					bw.write("valid\n");
 				}
@@ -72,7 +79,8 @@ public class Main {
 		br.close();
 		bw.close();
 	}
-
+	
+	//대각선 판별
 	private static boolean diagonal(String ST) {
 		if (maps[0][0].equals(maps[1][1]) && maps[1][1].equals(maps[2][2]) && maps[2][2].equals(ST)) {
 			return true;
@@ -81,7 +89,8 @@ public class Main {
 		}
 		return false;
 	}
-
+	
+	//세로 판별
 	private static boolean col(String ST) {
 		for (int c = 0; c < maps.length; c++) {
 			if (maps[0][c].equals(maps[1][c]) && maps[1][c].equals(maps[2][c]) && maps[2][c].equals(ST)) {
@@ -90,14 +99,14 @@ public class Main {
 		}
 		return false;
 	}
-
+	
+	//가로 판별
 	private static boolean row(String ST) {
 		for (int r = 0; r < maps.length; r++) {
 			if (maps[r][0].equals(maps[r][1]) && maps[r][1].equals(maps[r][2]) && maps[r][2].equals(ST)) {
 				return true;
 			}
 		}
-
 		return false;
 	}
 
