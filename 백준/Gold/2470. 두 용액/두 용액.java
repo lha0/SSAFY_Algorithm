3a80dcc004190ef13f98e9a51f279611b32ce6e1
이하영 <78598160+lha0@users.noreply.github.com>
2024-10-30 01:04:39 +0900

    [Gold V] Title: 두 용액, Time: 276 ms, Memory: 31708 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -3,27 +3,32 @@ import java.util.*;
 
 public class Main {
 	static int N;
-	static int[] liquid, answer;
+	static long minNum;
+	static long[] liquid, answer;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		N = Integer.parseInt(br.readLine());
-		liquid = new int[N];
+		liquid = new long[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			liquid[i] = Integer.parseInt(st.nextToken());
+			liquid[i] = Long.parseLong(st.nextToken());
 		}
 		
 		Arrays.sort(liquid);
 		
-		answer = new int[2];
+		answer = new long[2];
+		minNum = Long.MAX_VALUE;
 		
 		run();
+		
+		Arrays.sort(answer);
 		
 		for (int i = 0; i < 2; i++) {
 			bw.write(answer[i] + " ");
 		}
+		
 		bw.write("\n");
 		br.close();
 		bw.close();
@@ -31,34 +36,25 @@ public class Main {
 	
 	public static void run() {
 		int start = 0;
-		int end = N-1;
-		int sum = liquid[start] + liquid[end];
-		int diff = Math.abs(0 - Math.abs(sum));
-		
-		int minDiff = diff;
-		
-		answer = new int[] {liquid[start], liquid[end]};
-		
-		while (start < end-1) {
-			// start+1을 하는게 작을지, end-1을 하는게 작을지 미리 판단 후 작은 방향으로 포인터 이동
-			if (Math.abs(liquid[start+1] + liquid[end]) < Math.abs(liquid[start] + liquid[end-1])) {
-				sum -= liquid[start];
-				start++;
-				sum += liquid[start];
-				diff = Math.abs(0 - Math.abs(sum));
-			} else {
-				sum -= liquid[end];
-				end--;
-				sum += liquid[end];
-				diff = Math.abs(0 - Math.abs(sum));
-			}
-			
-			// 최소 차이보다 작다면 답 갱신
-			if (diff < minDiff) {
-				answer = new int[] {liquid[start], liquid[end]};
-				minDiff = diff;
-			}
-		}
+        int end = N - 1;
+        
+        while (start < end) {
+            long sum =  liquid[start] + liquid[end];
+            long diff = Math.abs(sum);
+            
+            if (diff < minNum) {
+                minNum = diff;
+                answer[0] = liquid[start];
+                answer[1] = liquid[end];
+            }
+            
+            // 합계가 0보다 작으면 start 포인터를 올리고, 크면 end 포인터를 내림
+            if (sum < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
 		
 	}
 
