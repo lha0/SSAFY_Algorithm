e6abbd2ce1471b10499bcc4690cf996be14919b4
이하영 <78598160+lha0@users.noreply.github.com>
2025-01-10 09:38:42 +0900

    [Gold II] Title: 공항, Time: 232 ms, Memory: 26984 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -27,32 +27,30 @@ public class Main {
 
     public static void run() {
         int[] gate = new int[G+1];
+        //union-find 초기화
         for (int i = 0; i < gate.length; i++) {
             gate[i] = i;
         }
 
         for (int i = 0; i < plane.length; i++) {
             int curPlane = plane[i];
-//            System.out.println("curPlane is " + curPlane);
-
             int parent = find(gate, curPlane);
+            
             if (parent-1 < 0) return;
-
-//            System.out.println("parent is " + parent);
+            
+            //게이트 할당
+            //원하는 게이트에 비행기가 있다면, 그 앞 게이트에 할당
             if (union(gate, parent, parent-1)) {
                 answer++;
             } else {
                 return;
             }
-
-//            System.out.println(Arrays.toString(gate));
         }
     }
 
     public static int find(int[] gate, int a) {
         if (gate[a] == a) return a;
         return gate[a] = find(gate, gate[a]);
-
     }
 
     public static boolean union(int[] gate, int a, int b) {
