7313b1acee5c3966ec1cb74ea42d75b296c425e9
이하영 <78598160+lha0@users.noreply.github.com>
2025-01-10 09:36:33 +0900

    [Gold II] Title: 공항, Time: 236 ms, Memory: 26860 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int answer, G, P;
+    static int[] plane;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        G = Integer.parseInt(br.readLine().split(" ")[0]);
+        P = Integer.parseInt(br.readLine().split(" ")[0]);
+
+        plane = new int[P];
+        for (int i = 0; i < P; i++) {
+            int g = Integer.parseInt(br.readLine().split(" ")[0]);
+            plane[i] = g;
+        }
+
+        run();
+
+        bw.write(answer + "\n");
+
+        br.close();
+        bw.close();
+    }
+
+    public static void run() {
+        int[] gate = new int[G+1];
+        for (int i = 0; i < gate.length; i++) {
+            gate[i] = i;
+        }
+
+        for (int i = 0; i < plane.length; i++) {
+            int curPlane = plane[i];
+//            System.out.println("curPlane is " + curPlane);
+
+            int parent = find(gate, curPlane);
+            if (parent-1 < 0) return;
+
+//            System.out.println("parent is " + parent);
+            if (union(gate, parent, parent-1)) {
+                answer++;
+            } else {
+                return;
+            }
+
+//            System.out.println(Arrays.toString(gate));
+        }
+    }
+
+    public static int find(int[] gate, int a) {
+        if (gate[a] == a) return a;
+        return gate[a] = find(gate, gate[a]);
+
+    }
+
+    public static boolean union(int[] gate, int a, int b) {
+        int parentA = find(gate, a);
+        int parentB = find(gate, b);
+
+        if (parentA == parentB) return false;
+        else {
+            if (parentA < parentB) {
+                gate[parentB] = parentA;
+            } else {
+                gate[parentA] = parentB;
+            }
+            return true;
+        }
+    }
+
+}
