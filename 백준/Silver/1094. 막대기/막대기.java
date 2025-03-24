c4ccfa2523052e91b1c57c0c51369d3ec7909084
이하영 <78598160+lha0@users.noreply.github.com>
2025-03-24 20:11:00 +0900

    [Silver V] Title: 막대기, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,13 +1,11 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int X;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        X = Integer.parseInt(br.readLine());
+        int X = Integer.parseInt(br.readLine());
 
         int count = 1;
         int bar = 64;
